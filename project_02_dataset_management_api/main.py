888062b8c046d3d640adbb8f9b67845c7a7e241b
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-09-15 16:10:29 +0500

    feat(project_2): create a route to check the health of the api

--- project_02_dataset_management_api/main.py
+++ project_02_dataset_management_api/main.py
@@ -1,8 +1,11 @@
 # Create an api to store, view and delete data stored about datasets in sqlite3 db
 from fastapi import FastAPI
+from routers import health
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return("Hello: World")
+
+app.include_router(health.router)
